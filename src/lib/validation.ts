5812657aba485053d1bcf491c5179950f5d5d8e7
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-11-15 09:49:19 +0300

    Added the sign in validation

--- src/lib/validation.ts
+++ src/lib/validation.ts
@@ -13,3 +13,16 @@ export const SignupSchema = z
   });
 
 export type SignUpType = z.infer<typeof SignupSchema>;
+
+export const signInSchema = z
+  .object({
+    email: z.string().min(1, "Email is required").email("Invalid email"),
+    password: z.string().min(6, "Password should be at least 6 characters"),
+    confirmPassword: z.string(),
+  })
+  .refine((data) => data.password === data.confirmPassword, {
+    message: "Passwords do not match",
+    path: ["confirmPassword"],
+  });
+
+export type SignInType = z.infer<typeof signInSchema>;
