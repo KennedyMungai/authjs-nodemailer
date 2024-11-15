cce46819601085a446fd8a5153734412085a3f01
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-11-15 15:27:02 +0300

    Added a table containing the user info

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,13 +1,35 @@
+import { auth } from "@/auth";
 import { protectServer } from "@/lib/session-hooks";
 
 const ProfilePage = async () => {
   await protectServer();
+
+  const session = await auth();
 
   return (
     <main className="mt-4">
       <div className="container mx-auto">
         <h1 className="text-3xl font-bold tracking-tight">Profile</h1>
         <div className="my-4 h-0.5 bg-muted" />
+        <>
+          <h2 className="text-2xl font-bold tracking-tight">
+            User Information
+          </h2>
+          <table className="mt-4 table-auto divide-y">
+            <thead>
+              <tr className="divide-x">
+                <th className="bg-gray-50 px-6 py-3 text-start">Name</th>
+                <th className="bg-gray-50 px-6 py-3 text-start">Email</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr className="divide-x">
+                <th className="px-6 py-3">{session?.user?.name}</th>
+                <th className="px-6 py-3">{session?.user?.email}</th>
+              </tr>
+            </tbody>
+          </table>
+        </>
       </div>
     </main>
   );
