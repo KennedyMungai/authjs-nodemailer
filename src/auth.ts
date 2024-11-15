4effb59d73f735d2983f198391700577081c1bb9
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-11-15 10:15:32 +0300

    Base database setup

---
+++ src/auth.ts
@@ -0,0 +1,10 @@
+import NextAuth from "next-auth";
+import { db } from "@/db";
+import { DrizzleAdapter } from "@auth/drizzle-adapter";
+import authConfig from "@/auth.config";
+
+export const {} = NextAuth({
+  adapter: DrizzleAdapter(db),
+  session: { strategy: "jwt" },
+  ...authConfig,
+});
