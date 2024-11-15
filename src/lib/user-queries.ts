816d8cf946662fab2ed9195fedcb6930380d5005
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-11-15 13:03:49 +0300

    Defined the hook for looking up users in the database

---
+++ src/lib/user-queries.ts
@@ -0,0 +1,10 @@
+import { db } from "@/db";
+import { users } from "@/db/schema";
+import { eq } from "drizzle-orm";
+
+export const findUserByEmail = async (email: string) =>
+  await db
+    .select()
+    .from(users)
+    .where(eq(users.email, email))
+    .then((data) => data[0]);
