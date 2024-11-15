a6f7cfc54f31792fd72148307c19c598101dc8a2
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-11-15 09:17:10 +0300

    Created the validation ts file and added the schema used to sign up

---
+++ src/lib/validation.ts
@@ -0,0 +1,15 @@
+import { z } from "zod";
+
+export const SignupSchema = z
+  .object({
+    name: z.string().min(1, "Name is required"),
+    email: z.string().min(1, "Email is required").email("Invalid email"),
+    password: z.string().min(6, "Password should be at least 6 characters"),
+    confirmPassword: z.string(),
+  })
+  .refine((data) => data.password === data.confirmPassword, {
+    message: "Passwords do not match",
+    path: ["confirmPassword"],
+  });
+
+export type SignUpType = z.infer<typeof SignupSchema>;
