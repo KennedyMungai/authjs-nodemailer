82199fcbb467e1735737f0e3dad19042411c6546
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-11-15 09:12:06 +0300

    Added some markup and logic

--- src/app/auth/signin/page.tsx
+++ src/app/auth/signin/page.tsx
@@ -1,5 +1,14 @@
 const SigninPage = () => {
-  return <div>SigninPage</div>;
+  return (
+    <main className="mt-4">
+      <div className="container mx-auto">
+        <h1 className="text-3xl font-bold tracking-tight">Sign In</h1>
+        <div className="my-4 h-0.5 bg-muted" />
+
+        {/* TODO: Add signin form */}
+      </div>
+    </main>
+  );
 };
 
 export default SigninPage;
--- src/app/auth/signup/page.tsx
+++ src/app/auth/signup/page.tsx
@@ -1,5 +1,14 @@
 const SignupPage = () => {
-  return <div>SignupPage</div>;
+  return (
+    <main className="mt-4">
+      <div className="container mx-auto">
+        <h1 className="text-3xl font-bold tracking-tight">Sign Up</h1>
+        <div className="my-4 h-0.5 bg-muted" />
+
+        {/* TODO: Add signup form */}
+      </div>
+    </main>
+  );
 };
 
 export default SignupPage;
