2b9a012050a1dd692ed7ee6f79b00ebf833f4317
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-11-16 11:47:19 +0300

    Using the specific user tables for the drizzle adapter

--- src/auth.ts
+++ src/auth.ts
@@ -2,9 +2,16 @@ import authConfig from "@/auth.config";
 import { db } from "@/db";
 import { DrizzleAdapter } from "@auth/drizzle-adapter";
 import NextAuth from "next-auth";
+import * as schema from "@/db/schema";
 
 export const { auth, handlers, signIn, signOut } = NextAuth({
-  adapter: DrizzleAdapter(db),
+  adapter: DrizzleAdapter(db, {
+    accountsTable: schema.accounts,
+    usersTable: schema.users,
+    authenticatorsTable: schema.authenticators,
+    sessionsTable: schema.sessions,
+    verificationTokensTable: schema.verificationTokens,
+  }),
   session: { strategy: "jwt" },
   pages: { signIn: "/auth/signIn" },
   callbacks: {
