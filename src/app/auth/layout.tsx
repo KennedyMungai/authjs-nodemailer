e1d2b9d16b3b6cf065630609bbca5f9ad463e48a
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-11-18 10:10:55 +0300

    Removed the user verification logic

--- src/app/auth/layout.tsx
+++ src/app/auth/layout.tsx
@@ -1,16 +1,10 @@
-import { auth } from "@/auth";
-import { redirect } from "next/navigation";
 import { ReactNode } from "react";
 
 type Props = {
   children: ReactNode;
 };
 
-const AuthLayout = async ({ children }: Props) => {
-  const session = await auth();
-
-  if (session) redirect("/");
-
+const AuthLayout = ({ children }: Props) => {
   return <div className="h-full">{children}</div>;
 };
 
