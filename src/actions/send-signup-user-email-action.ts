0ce4b5798ab53c15d70d864d1bc79ed15d87421f
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-11-18 15:46:30 +0300

    Updated the html that would be sent as the email

--- src/actions/send-signup-user-email-action.ts
+++ src/actions/send-signup-user-email-action.ts
@@ -18,7 +18,9 @@ export const sendSignupUserEmailAction = async ({
     to: email,
     subject: "Verify your email address",
     html: `
-        <div>
+        <div style="font-family: Arial, sans-serif; color: #333; line-height: 1.6; max-width: 600px; margin: auto; padding: 20px; border: 1px solid #eaeaea; border-radius: 10px;">
+            <h2 style="text-align: center; color: #3b82f6;">Team Rocket</h2>
+            <p>Hi there!</p>
             <p>
                 <a href="${process.env.NEXT_PUBLIC_APP_URL}/auth/signup/verify-email?token=${verificationToken}">Click here to verify your email address</a>
             </p>
